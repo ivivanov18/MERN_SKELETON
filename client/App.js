b8d79882048ecf5fa4f33541f392b0e0af3598c1
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-03-06 10:35:55 +0200

    feat: add App, Home component
    add webpack image bundling capacity

---
+++ client/App.js
@@ -0,0 +1,41 @@
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { indigo, pink } from "@material-ui/core/colors";
+import React from "react";
+import MainRouter from "./MainRouter";
+import { BrowserRouter } from "react-router-dom";
+import { hot } from "react-hot-loader";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#757de8",
+      main: "#3f51b5",
+      dark: "#002984",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff79b0",
+      main: "#ff4081",
+      dark: "#c60055",
+      contrastText: "#000"
+    },
+    openTitle: indigo["400"],
+    protectedTitle: pink["400"],
+    type: "light"
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
+const App = () => (
+  <BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <MainRouter />
+    </MuiThemeProvider>
+  </BrowserRouter>
+);
+
+export default hot(module)(App);
+//export default hot(App);
+//export default App;
---
+++ client/core/Home.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles, createMuiTheme } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    margin: "auto",
+    marginTop: theme.spacing.unit * 5
+  },
+  title: {
+    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px
+    ${theme.spacing.unit * 2}px`,
+    color: theme.palette.text.secondary
+  },
+  media: {
+    minHeight: 330
+  }
+});
+
+class Home extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="HOMEPAGE"
+        />
+        <CardContent>
+          Text
+          <CardMedia
+            className={classes.media}
+            src="/assets/images/seashell.jpg"
+            image="/assets/images/seashell.jpg"
+            title="Sea Shell"
+          />
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(Home);
+//export default Home;
